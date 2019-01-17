873b622fd565d0fcae0ef9ebb6801d27b45b3175
RianaM <2541354+AngelicBrighteyes@users.noreply.github.com>
2019-01-16 19:30:00 -0700

    .Net Pact Workshop: Step 3 completed

---
+++ YourSolution/Consumer/tests/ConsumerPactClassFixture.cs
@@ -0,0 +1,72 @@
+﻿using PactNet;
+using PactNet.Mocks.MockHttpService;
+using System;
+using Xunit;
+
+namespace tests
+{
+    /* This class is responsible for setting up a shared
+     * mock server for Pact used by all the tests.
+     * Xunit can use a Class Fixture for this.
+     * See: https://goo.gl/hSq4nv
+     */
+    public class ConsumerPactClassFixture : IDisposable
+    {
+        public IPactBuilder PactBuilder { get; private set; }
+        public IMockProviderService MockProviderService { get; private set; }
+
+        public int MockServerPort { get { return 9222; } }
+
+        public string MockProviderServiceBaseUri
+        {
+            get
+            {
+                return String.Format("http://localhost:{0}", this.MockServerPort);
+            }
+        }
+
+        public ConsumerPactClassFixture()
+        {
+            // Using spec version 2.0.0 more details at https://goo.gl/UrBSRc
+            var pactConfig = new PactConfig
+            {
+                SpecificationVersion =  "2.0.0",
+                PactDir = @"..\..\..\..\..\pacts",
+                LogDir = @".\pact_logs"
+            };
+
+            this.PactBuilder = new PactBuilder(pactConfig);
+
+            this.PactBuilder.ServiceConsumer("consumer")
+                .HasPactWith("Provider");
+
+            this.MockProviderService = PactBuilder.MockService(this.MockServerPort);
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // this will save the pact file once finished
+                    PactBuilder.Build();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // do not change this code. put cleanup code in Dispose(bool disposing)
+            Dispose(true);
+        }
+        #endregion IDisposable Support
+    }
+}
---
+++ YourSolution/Consumer/tests/ConsumerPactTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xunit;
+using PactNet.Mocks.MockHttpService;
+using PactNet.Mocks.MockHttpService.Models;
+using Consumer;
+using System.Collections.Generic;
+
+namespace tests
+{
+    public class ConsumerPactTests : IClassFixture<ConsumerPactClassFixture>
+    {
+        private IMockProviderService mockProviderService;
+        private string mockProviderServiceBaseUri;
+
+        public ConsumerPactTests(ConsumerPactClassFixture fixture)
+        {
+            this.mockProviderService = fixture.MockProviderService;
+            this.mockProviderService.ClearInteractions(); // NOTE: Clears any previously registered interactions before the test is rn
+            this.mockProviderServiceBaseUri = fixture.MockProviderServiceBaseUri;
+        }
+
+        [Fact]
+        public void ItHandlesInvalidDateParams()
+        {
+            // arange
+            var invalidRequestMessage = "validDateTime is not a date or time";
+            this.mockProviderService.Given("There is data")
+                .UponReceiving("An invalid GET request for Date Validation with an invalid date parameter")
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Get,
+                    Path = "/api/provider",
+                    Query = "validDateTime=lolz"
+                })
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 400,
+                    Headers = new Dictionary<string, object>
+                    {
+                        {"content-type", "application/json; charset=utf-8" }
+                    },
+                    Body = new
+                    {
+                        message = invalidRequestMessage
+                    }
+                });
+
+            // act
+            var result = ConsumerApiClient
+                .ValidateDateTimeUsingProviderApi("lolz", mockProviderServiceBaseUri)
+                .GetAwaiter()
+                .GetResult();
+            var resultBodyText = result.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+            // assert
+            Assert.Contains(invalidRequestMessage, resultBodyText);
+        }
+    }
+}
